a36f2cab7ea28228a97256de4cf5643b84ee5361
alexander-farrell <irishsnowday@icloud.com>
2024-01-30 23:02:56 -0500

    rollback to setup portion

--- server/core/template.js
+++
@@ -1,5 +0,0 @@
-export function render(req, res, name, title, data={}) {
-    data['title'] = `${title} | Production Server`;
-    data['content'] = name;
-    res.render("template", data)
-}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,7 @@
 import { Router } from "express";
-import { render } from "./../core/template.js";
 
 export const index_router = Router()
 
 index_router.get('/', (req, res) => {
-    render(req, res, 'index.ejs', 'Home');
-})
-
-index_router.get('/about', (req, res) => {
-    render(req, res, 'about.ejs', 'About');
-})
-
-index_router.get('/contact', (req, res) => {
-    render(req, res, 'contact.ejs', 'Contact');
+    res.send("Hello")
 })
